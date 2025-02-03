1ffd46267aac4f46308a388d4b94a7e0f23c4f8a
Dasha L <haskandasha@mail.ru>
2025-02-03 23:21:40 +0300

    edit functions

--- src/masks.py
+++ src/masks.py
@@ -1,20 +1,20 @@
-def get_mask_card_number(card_number: int) -> str:
+def get_mask_card_number(card_number: str) -> str:
     """
-    Принимает на вход номер карты в виде числа и возвращает маску номера
+    Принимает на вход номер карты и возвращает маску номера
     по правилу XXXX XX** **** XXXX
     """
-    card_number_str = str(card_number)
-    return f"{card_number_str[:4]} {card_number_str[4:6]}** ****{card_number_str[-4:]}"
+
+    return f"{card_number[:4]} {card_number[4:6]}** ****{card_number[-4:]}"
 
 
 def get_mask_account(account_name: str) -> str:
     """
-    Принимает на вход номер счета в виде числа и возвращает маску номера
+    Принимает на вход номер счета и возвращает маску номера
     по правилу **XXXX
     """
-    return f"**{account_name[1:]}"
+    return f"**{account_name[-4:]}"
 
 
 if __name__ == "__main__":
     print(get_mask_card_number(12345678910121314))
-    print(get_mask_account("123456"))
+    print(get_mask_account("73654108430135874305"))
