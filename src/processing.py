837b97a987c8ade58b81e5db3daa902bd87fecb1
Dasha-L <dl274274@hotmail.com>
2025-02-09 00:39:22 +0300

    sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,21 @@
+from src.widget import get_date
+from datetime import datetime
+
+
 def filter_by_state(transactions: list[dict], state: str = 'EXECUTED') -> list[dict]:
     """" Фильтрует список транзакций по статусу выполнения """
     return [t for t in transactions if t['state'] == state]
+
+
+def sort_by_date(transactions: list[dict], reverse: bool = True) -> list[dict]:
+    """ Сортирует список транзакций по дате."""
+
+    def convert_date(d: dict) -> datetime:
+        """ Преобразует дату из словаря в объект datetime для сортировки."""
+        date_str = get_date(d['date'])
+        return datetime.strptime(date_str, '%d.%m.%Y')
+
+    return sorted(transactions, key=convert_date, reverse=reverse)
 
 
 if __name__ == '__main__':
@@ -11,4 +26,4 @@ if __name__ == '__main__':
                     ]
 
     print(filter_by_state(transactions))
-    #print(sort_by_date(transactions))
+    print(sort_by_date(transactions))
