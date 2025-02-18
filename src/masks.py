342d7082ff8b7129a06c6fdeeaf6d2a3f9287106
Dasha-L <dl274274@hotmail.com>
2025-02-18 22:52:42 +0300

    Added new function requirements

--- src/masks.py
+++ src/masks.py
@@ -1,20 +1,31 @@
 def get_mask_card_number(card_number: str) -> str:
     """
-    Принимает на вход номер карты и возвращает маску номера
+    Принимает на вход номер карты от 13 до 19 символов и возвращает маску номера
     по правилу XXXX XX** **** XXXX
     """
+    if not card_number.isdigit():
+        raise ValueError("Номер карты должен содержать только цифры")
+    if len(card_number) < 13 or len(card_number) > 19:
+        raise ValueError(f"Номер карты должен быть длиной от 13 до 19 символов,"
+                         f" передан номер длиной {len(card_number)}")
+    else:
+        return f"{card_number[:4]} {card_number[4:6]}** ****{card_number[-4:]}"
 
-    return f"{card_number[:4]} {card_number[4:6]}** ****{card_number[-4:]}"
 
-
-def get_mask_account(account_name: str) -> str:
+def get_mask_account(account_number: str) -> str:
     """
     Принимает на вход номер счета и возвращает маску номера
     по правилу **XXXX
     """
-    return f"**{account_name[-4:]}"
+    if not account_number.isdigit():
+        raise ValueError("Номер счёта должен содержать только цифры")
+    if len(account_number) < 6:
+        raise ValueError(f"Номер счета должен быть длиной от 6 символов,"
+                         f" передан номер длиной {len(account_number)}")
+    else:
+        return f"**{account_number[-4:]}"
 
 
 if __name__ == "__main__":
-    print(get_mask_card_number("1596837868705199"))
+    print(get_mask_card_number("1596837868705"))
     print(get_mask_account("73654108430135874305"))
